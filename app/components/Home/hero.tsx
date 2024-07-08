27f2b17f6b033aa6ca1a96069cda12151b5fcaf7
Oluwademilade Ala <demiladeala@gmail.com>
2024-07-08 21:37:20 +0100

    feat: add hero section

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -1,8 +1,14 @@
 export const Hero = () => {
     return (
-        <section className="">
+        <section className="text-white">
             <div className="relative isolate overflow-hidden h-[42rem] bg-black-500">
-                
+                <div className="w-[93%] md:w-[90%] mx-auto flex flex-col md:flex-row md:items-center gap-10">
+                    <div className="w-full md:basis-[50%]">
+                        <h1 className="font-semibold text-3xl md:text-4xl lg:text-5xl md:w-[80%] border">
+                            Shape Your Gaming Career Pathway
+                        </h1>
+                    </div>
+                </div>
             </div>
         </section>
     )
