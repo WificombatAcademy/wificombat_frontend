e53acd0392ce82653e96aec2dadd33631944257f
Oluwademilade Ala <demiladeala@gmail.com>
2024-07-08 21:07:06 +0100

    feat: add hero section

---
+++ app/components/Home/hero.tsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+    return (
+        <section className="">
+            <div className="relative isolate overflow-hidden h-[42rem] bg-black-500">
+                
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/general/navbar";
+import { Hero } from "./components/Home/hero";
 
 export default function Home() {
   return (
     <div className="mx-auto relative container w-full max-w-[2000px]">
       <Navbar />
+      <Hero />
     </div>
   );
 }
