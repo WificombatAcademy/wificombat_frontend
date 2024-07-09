d45e1fdf025fa8951f73f1e3feaa3243b73eca43
Oluwademilade Ala <demiladeala@gmail.com>
2024-07-09 02:09:01 +0100

    feat: add hero section

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -37,22 +37,22 @@ export const Hero = () => {
             slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
-            className="relative text-white w-full"
+            className="relative text-white w-full h-full"
             >
                 {slides.map((slide, index) => (
-                    <SwiperSlide key={index} className="relative w-[93%] md:w-[90%] mx-auto">
+                    <SwiperSlide key={index} className="relative">
                         <div className={`relative isolate overflow-hidden pb-[7rem] md:py-[7rem] lg:py-[10rem] ${slide.bgColor}`}>
-                            <div className="relative max-md:mt-[4rem] w-[93%] md:w-[85%] mx-auto flex flex-col md:flex-row md:items-center gap-10">
+                            <div className="relative max-md:mt-[4rem] w-[93%] md:w-[80%] lg:w-[85%] mx-auto h-[16rem] xl:h-[20rem] 2xl:h-[24rem] flex flex-col md:flex-row md:items-center gap-10">
                                 <div className="w-full md:basis-[50%]">
-                                    <h1 className="font-semibold text-3xl md:text-4xl lg:text-5xl md:w-[85%]">
+                                    <h1 className="font-semibold text-3xl md:text-4xl lg:text-5xl">
                                         {slide.heading1}
                                     </h1>
 
-                                    <p className="mt-5 text-lg md:text-xl h-[7.4rem] md:h-[5rem]">
+                                    <p className="mt-5 text-lg md:text-xl">
                                         {slide.heading2}
                                     </p>
                                     
-                                    <div className="mt-9 md:mt-14">
+                                    <div className="mt-10">
                                         {slide.buttonBlack ? 
                                         <>
                                         <Link
