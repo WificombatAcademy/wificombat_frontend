1eef9135e3cf7f452e9c46bd6d7e6a7d3fdbfe92
Oluwademilade Ala <demiladeala@gmail.com>
2024-07-10 03:36:07 +0100

    feat: add community section

--- app/components/Home/testimonials.tsx
+++ app/components/Home/testimonials.tsx
@@ -11,7 +11,7 @@ const testimonialImages = [
 
 export const Testimonials = () => {
     return (
-        <div className="w-full py-7 md:py-10 pb-52 lg:pb-48 bg-black-500">
+        <div className="w-full py-7 md:py-10  bg-black-500">
             <div className="w-[93%] md:w-[88%] mx-auto mt-3 flex max-[330px]:flex-col 
             flex-row items-center justify-center gap-2 md:gap-4">
                 <div className="bg-white py-4 px-5 font-semibold text-lg md:text-2xl rounded-3xl">
@@ -33,7 +33,7 @@ export const Testimonials = () => {
                     <h2 className="mt-6 md:mt-8 text-2xl md:text-3xl font-semibold">Esther Howard</h2>
                     <h4 className="mt-5 text-lg md:text-xl">Alumni</h4>
 
-                    <div className="mt-14 flex items-center justify-center">
+                    <div className="mt-14 mb-40 flex items-center justify-center">
                         <div className="flex items-center gap-2 md:gap-6">
                             {testimonialImages.map((image, index) => (
                                 <div key={index} className="max-[350px]:w-[55px] max-[350px]:h-[55px] w-[60px] h-[60px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px] rounded-full">
