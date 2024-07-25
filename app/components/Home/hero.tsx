8032b849c7e79ceee51b3f5686eb28fced1b6074
Oluwademilade Ala <demiladeala@gmail.com>
2024-07-25 15:58:27 +0100

    feat: add pages edits

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -7,31 +7,32 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import { merriweather } from "@/app/fonts";
+import Image from "next/image";
 
 const slides = [
     {heading1: "Discover Your Coding Career Pathway", 
      heading2: `Turn your passion for technology into a thriving career in software engineering or DevOps. Begin your path with us`,
-    bgColor: "bg-blue-500", buttonBlack: true },
+    bgColor: "bg-blue-500", buttonBlack: true, img:"/hero-1.png" },
 
     {heading1: "Shape Your Gaming Career Pathway", 
     heading2: `Ready to turn your passion for gaming into a career? Dive into the world of game development and start your game career pathway`,
-    bgColor: "bg-blue-300", buttonBlack: true },
+    bgColor: "bg-blue-300", buttonBlack: true, img:"/hero-2.png" },
 
     {heading1: "Explore the Multimedia Pathway", 
     heading2: `Your Journey in Multimedia Starts Here! Join our program to become a skilled Animator, Illustrator, and 2D & 3D Designer. Shape your creative future with us.`,
-    bgColor: "bg-purple-500", buttonBlack: true },
+    bgColor: "bg-purple-500", buttonBlack: true, img:"" },
 
     {heading1: "Navigate the AI Career Pathway", 
     heading2: `Dive into the exciting world of artificial intelligence. Gain expertise, innovate, and lead the way in AI technology with our specialized career pathway.`,
-    bgColor: "bg-black-500", buttonBlack: false },
+    bgColor: "bg-black-500", buttonBlack: false, img: "" },
 
     {heading1: "Dive into the Robotics and IoT Career Pathway", 
     heading2: `Step into the world of Robotics and IoT with our exclusive career pathway program. Design and implement groundbreaking technologies`,
-    bgColor: "bg-blue-500", buttonBlack: true },
+    bgColor: "bg-blue-500", buttonBlack: true, img:"" },
 
     {heading1: "Navigate the  Techprenuership Career Pathway", 
      heading2: `Lorem ipsum dolor sit amet consectetur. Nunc et aenean imperdiet dignissim suspendisse in. Pretium ante adipiscing sed amet eget sed in. Parturient t`,
-    bgColor: "bg-yellow-500", buttonBlack: true },
+    bgColor: "bg-yellow-500", buttonBlack: true, img:"" },
 ]
 
 export const Hero = () => {
@@ -50,11 +51,11 @@ export const Hero = () => {
             >
                 {slides.map((slide, index) => (
                     <SwiperSlide key={index} className="relative">
-                        <div className={`relative isolate overflow-hidden pb-[7rem] md:py-[7rem] lg:py-[10rem] ${slide.bgColor}`}>
-                            <div className="relative max-md:mt-[4rem] w-[93%] md:w-[80%] lg:w-[80%] mx-auto h-[16rem] xl:h-[20rem] 2xl:h-[24rem] flex flex-col md:flex-row md:items-center gap-10">
+                        <div className={`relative isolate overflow-x-hidden pb-[7rem] md:py-[7rem] lg:py-[10rem] ${slide.bgColor}`}>
+                            <div className="relative h-fit max-md:mt-[4rem] w-[93%] md:w-[80%] lg:w-[80%] mx-auto md:h-[16rem] xl:h-[20rem] 2xl:h-[24rem] flex flex-col md:flex-row md:items-center gap-10">
                                 <div className="w-full md:basis-[50%]">
                                     <h1 className={`${merriweather.className} 
-                                    text-3xl md:text-4xl lg:text-5xl lg:w-[90%] leading-[40px] lg:leading-[67.2px] font-bold`}>
+                                    text-3xl md:text-4xl lg:text-5xl xl:w-[90%] max-md:leading-[45px] lg:leading-[67.2px] font-bold`}>
                                         {slide.heading1}
                                     </h1>
 
@@ -62,7 +63,7 @@ export const Hero = () => {
                                         {slide.heading2}
                                     </p>
                                     
-                                    <div className="mt-10 lg:mt-16">
+                                    <div className="mt-12 lg:mt-16">
                                         <Link
                                             href="/registration"
                                             className={`
@@ -75,6 +76,19 @@ export const Hero = () => {
                                         </Link>
                                     </div>
                                 </div>
+
+                                <div className="w-full md:basis-[50%] max-md:hidden">
+                                    {slide.img !== ""
+                                     && 
+                                    <Image 
+                                    src={slide.img ?? ""}
+                                    alt={slide.img ?? ""}
+                                    width={500}
+                                    height={500}
+                                    className="w-full h-full object-cover border-none"
+                                    />}
+                                </div>
+
                             </div>
                         </div>
                     </SwiperSlide>
