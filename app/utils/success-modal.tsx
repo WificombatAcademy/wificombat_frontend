a9e3bb8ae6f4aac7651155f4e59b04b9376175f1
Oluwademilade Ala <demiladeala@gmail.com>
2024-08-07 14:18:01 +0100

    feat: add signup page

--- app/utils/success-modal.tsx
+++ app/utils/success-modal.tsx
@@ -15,15 +15,17 @@ const SuccessModal = ({isOpen, onClose}: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} removeCancelIcon={true}>
 
         <Image 
         src={`/assets/auth/success.svg`}
         alt='success'
-        className='object-contain mx-auto'
+        width={91}
+        height={87}
+        className='mt-7 object-contain mx-auto'
         />
 
-        <div className="text-xl font-semibold text-center">
+        <div className="mt-6 text-xl font-semibold text-center">
         Verification Successful
         </div>
         <p className="text-lg font-medium my-4 text-center">
@@ -33,7 +35,7 @@ const SuccessModal = ({isOpen, onClose}: ModalProps) => {
 
         <Link href={`/create-profile`}>
             <button
-            className="flex w-full justify-center rounded-md disabled:bg-[#B1B1B4] active:bg-[#131314] bg-[#131314] 
+            className="mt-10 flex w-full justify-center rounded-md disabled:bg-[#B1B1B4] active:bg-[#131314] bg-[#131314] 
             p-4 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-500 focus-visible:outline 
             focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
             >
