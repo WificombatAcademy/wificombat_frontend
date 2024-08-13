b75dca80fb2e2190dc06553fc76aaa3cc2899e37
Oluwademilade Ala <demiladeala@gmail.com>
2024-08-13 09:27:01 +0100

    fix: add landing page edits

--- app/components/CodingPathwayComps/why-coding.tsx
+++ app/components/CodingPathwayComps/why-coding.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image"
+
+const pathwayImages = [
+    "/assets/pathway/coding-pathway-1.png",
+    "/assets/pathway/coding-pathway-2.png",
+    "/assets/pathway/coding-pathway-3.png",
+]
+
 export const WhyCodingPathway = () => {
     return (
         <section>
@@ -12,19 +20,21 @@ export const WhyCodingPathway = () => {
                     quam erat amet amet velit. Velit tortor vivamus aliquam at imperdiet commodo.</p>
 
                  <div className="mt-9 md:mt-16 flex flex-col md:flex-row justify-center gap-9 xl:gap-12">
-
-                    <div className="relative w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] lg:w-[19rem] lg:h-[22rem]">
-                        <div className="polygon bg-primary-gray w-full h-full"></div>
-                    </div>
-
-                    <div className="relative w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] lg:w-[19rem] lg:h-[22rem]">
-                        <div className="polygon bg-primary-gray w-full h-full"></div>
-                    </div>
-
-                    <div className="relative w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] lg:w-[19rem] lg:h-[22rem]">
-                        <div className="polygon bg-primary-gray w-full h-full"></div>
-                    </div>
-
+                    {pathwayImages.map((image, index) => (
+                        <div
+                        key={index}
+                        className="relative w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] lg:w-[19rem] lg:h-[22rem]">
+                            <div className="polygon w-full h-full">
+                                <Image 
+                                src={image}
+                                alt="skill"
+                                width={414}
+                                height={427}
+                                className="w-full h-full object-cover"
+                                />
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
---
+++ app/components/PricingComps/pricing-features.tsx
@@ -0,0 +1,9 @@
+export const PricingFeatures = () => {
+    return (
+        <div className="w-full py-16 bg-purple-500 overflow-x-scroll">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
