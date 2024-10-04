9756d247489e349a0a02f7279b8a4facf7da92fd
Oluwademilade Ala <demiladeala@gmail.com>
2024-10-04 11:59:03 +0100

    feat:add dashboard courses data

--- app/components/Dashboard/CareerPathway/lesson-content.tsx
+++ app/components/Dashboard/CareerPathway/lesson-content.tsx
@@ -1,3 +1,5 @@
+"use client"
+import Image from 'next/image';
 import React from 'react'
 import { RiLoader4Fill } from 'react-icons/ri';
 
@@ -9,6 +11,9 @@ type Props = {
     isQuizMode: boolean;
     loadingQuiz: boolean;
     fetchQuiz: () => void;
+    lessonTitle: string;
+    lessonIndex: number;
+    totalSlides: number;
 }
   
 
@@ -20,22 +25,50 @@ const LessonContent = ({
     isQuizMode, 
     loadingQuiz,
     fetchQuiz,
+    lessonTitle,
+    lessonIndex,
+    totalSlides,
     }: Props) => {
 
     console.log(selectedContent)
     
   return (
     <>
-    <div className="mt-5 p-5 h-[40vh] lg:h-[50vh] bg-white shadow overflow-y-auto rounded-3xl">
+    <div className="relative mt-5 p-5 h-[40vh] lg:h-[50vh] bg-white shadow overflow-y-auto rounded-3xl">
+        <div className="absolute bg-white inset-0 flex items-center justify-center">
+            <Image 
+            src={`/logo.png`}
+            alt='background'
+            width={40}
+            height={40}
+            className='w-full h-full object-contain invert-[100%] opacity-10'
+            />
+        </div>
         {selectedContent.length > 0 ? (
-            <div>
+            <div className='relative z-[1]'>
             {/* Display current slide */}
-            <div dangerouslySetInnerHTML={{ __html: selectedContent[currentSlide] }} />
+            <div 
+            className='bg-transparent'
+            dangerouslySetInnerHTML={{ __html: selectedContent[currentSlide] }} />
             </div>
         ) : (
-            <p>Select a lesson to view its content.</p>
+            <p className='m-auto text-center'>Select a lesson to view its content.</p>
         )}
-    </div>
+        </div>
+
+        {/* LESSON DETAILS */}
+        <div>
+            <div className='w-full flex items-center justify-between text-xl font-semibold'>
+                <h2 className="">{lessonTitle}</h2>
+                <p className="">
+                 {currentSlide + 1}/{totalSlides} Slides
+                </p>
+            </div>
+            <div>
+                <p className='text-black-600'> Lesson {lessonIndex}</p>
+            </div>
+        </div>
+        {/* LESSON DETAILS */}
         
         {/* Navigation Buttons */}
         <div className="lg:fixed lg:w-[52%] right-0 px-4 sm:px-6 lg:px-8 bottom-[5vh]">
