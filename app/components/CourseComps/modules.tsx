e30693653182bf049ac5e34645ddb1d9c7984ce4
Oluwademilade Ala <demiladeala@gmail.com>
2024-10-12 01:08:06 +0100

    feat:add dashboard courses data

--- app/components/CourseComps/modules.tsx
+++ app/components/CourseComps/modules.tsx
@@ -7,6 +7,12 @@ const Modules = (props: Props) => {
   return (
     <section>
         <HeadingDesign heading={`Modules`} noUppercase={true} />
+
+        <div className='mt-5'>
+            <p className='w-[90%] md:w-[70%] mx-auto text-lg md:text-xl text-center'>
+                You can buy the whole course and have access to all the modules 
+                or you can decide to buy the modules one after the other.</p>
+        </div>
     </section>
   )
 }
