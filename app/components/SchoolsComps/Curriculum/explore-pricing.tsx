aa5076e434fde9c8126e8578bfc922a34e6703b1
Yusuf Aniki <anikiyusuf99@gmail.com>
2024-11-04 19:48:46 +0100

    fix:error

--- app/components/SchoolsComps/Curriculum/explore-pricing.tsx
+++ app/components/SchoolsComps/Curriculum/explore-pricing.tsx
@@ -4,8 +4,9 @@ export const ExplorePricingPlan = () => {
     return (
         <section className="py-10 md:py-14 lg:py-20 bg-blue-50">
              <h3 className="w-[90%] mx-auto text-center text-2xl md:text-4xl font-semibold">Get a Quotation </h3>
-            <p className="mt-9 md:w-[80%] lg:w-[65%] mx-auto text-center md:text-xl">Don't wait! Contact us now to receive a free quote and learn more about our affordable solutions. </p>
-
+            <p className="mt-9 md:w-[80%] lg:w-[65%] mx-auto text-center md:text-xl">
+            Don&apos;t wait! Contact us now to receive a free quote and learn more about our affordable solutions.
+            </p>           
             <div className="mt-10 w-full flex items-center justify-center">
                 <Link
                   href={`/schools/pricing-plan`}
