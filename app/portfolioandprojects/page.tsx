8473672f85b8408cac95b712a1fd9fe1eb0a6a7b
Yusuf Aniki <anikiyusuf99@gmail.com>
2024-11-06 18:08:27 +0100

    fix:portfolio

--- app/portfolioandprojects/page.tsx
+++ app/portfolioandprojects/page.tsx
@@ -2,9 +2,9 @@ import GeneralNavbar from "../components/general/GeneralNavbar";
 import Footer from "@/app/components/general/Footer";
 import { StudentsHero } from "@/app/components/StudentsComps/Overview/hero";
 import { PortfolioBenefit } from "../components/PortfolioProject/keybenefit";
-import { PortfolioEnhance } from "../components/PortfolioProject/portfolio-enhance";
+// import { PortfolioEnhance } from "../components/PortfolioProject/portfolio-enhance";
 import { Prepare } from "../components/PortfolioProject/prepare";
-import { PortfolioManagement } from "../components/PortfolioProject/portfolio-management";
+// import { PortfolioManagement } from "../components/PortfolioProject/portfolio-management";
 export default function Page() {
     return (
         <div className="mx-auto relative container w-full max-w-[2000px]">
@@ -17,9 +17,9 @@ export default function Page() {
       btnColor={false}
       textColor={false}
       />
-      <PortfolioManagement/>
+      {/* <PortfolioManagement/> */}
       <PortfolioBenefit/>
-      <PortfolioEnhance/>
+      {/* <PortfolioEnhance/> */}
       <Prepare/>
       <Footer />
         </div>
